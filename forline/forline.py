cf75084cd5b37eb177479e30bdc206d92fdd11dc
Gilles Arcas <gilles.arcas@gmail.com>
2018-02-13 22:09:25 +0100

    Add main to be used as setup entry point.

--- forline/forline.py
+++ forline/forline.py
@@ -8,18 +8,6 @@ from __future__ import print_function, division
 import sys
 import getopt
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'b:e:s:t')
-    opts = dict(opts)
-except:
-    print(__doc__)
-    exit(1)
-
-head = opts['-b'] if '-b' in opts else 'pass'
-tail = opts['-e'] if '-e' in opts else 'pass'
-body = '\n    '.join(args)
-body = body if body.strip() else 'pass'
-sep = opts['-s'] if '-s' in opts else None
 
 SCRIPT = """\
 from __future__ import print_function, division
@@ -30,10 +18,29 @@ for num, line in enumerate(sys.stdin, 1):
     words = line.strip().split(%s)
     %s
 %s
-""" 
-script = SCRIPT % (head, sep, body, tail)
+"""
 
-if '-t' in opts:
-    print(script)
-else:
-    eval(compile(script, 'command', 'exec'), globals(), locals())
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'b:e:s:t')
+        opts = dict(opts)
+    except:
+        print(__doc__)
+        exit(1)
+
+    head = opts['-b'] if '-b' in opts else 'pass'
+    tail = opts['-e'] if '-e' in opts else 'pass'
+    body = '\n    '.join(args)
+    body = body if body.strip() else 'pass'
+    sep = opts['-s'] if '-s' in opts else None
+
+    script = SCRIPT % (head, sep, body, tail)
+
+    if '-t' in opts:
+        print(script)
+    else:
+        eval(compile(script, 'command', 'exec'), globals(), locals())
+
+    
+if __name__ == '__main__':
+    main()
