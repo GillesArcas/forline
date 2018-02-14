280e969c5f2ca90cea13c78f64e9039ec89f4b01
Gilles Arcas <gilles.arcas@gmail.com>
2018-02-14 21:34:28 +0100

    Add meta-data to setup.

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
-setup(name='forline',
+setup(
+    name='forline',
     version='1.0',
+    license='MIT'
     packages=['forline'],
+    url = 'https://github.com/GillesArcas/forline',
+    author = 'Gilles Arcas',
+    author_email = 'gilles.arcas@gmail.com',
     entry_points = {
         'console_scripts': ['forline=forline.forline:main'],
     },
